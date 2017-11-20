642a793b8614274e22362fe7c73810eed675ac35
Ron <ron@drongowski.de>
2017-11-20 09:44:21 +0100

    Setup basic skill

---
+++ src/zeit/alexa/skill.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+
+
+app = Flask(__name__,)
+ask = Ask(app, "/news")
+
+
+def get_headlines():
+    return 'Test headline'
+
+
+@ask.launch
+def start_skill():
+    welcome_message = 'Willst du die News wissen?'
+    return question(welcome_message)
+
+
+@ask.intent("YesIntent")
+def share_headlines():
+    headlines = get_headlines()
+    headline_msg = 'Die aktuellen Schlagzeilen sind: {}'.format(headlines)
+    return statement(headline_msg)
+
+
+@ask.intent("NoIntent")
+def no_intent():
+    bye_text = 'OK! Auf Wiedersehen!'
+    return statement(bye_text)
+
+
+def run(global_config, **settings):
+    app.settings = settings
+    return app
+
+
+factory = run
